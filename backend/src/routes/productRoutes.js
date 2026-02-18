c97997eb0c8da52e53a12b369466514ad4b9e965
Harshitpant12 <harshpant778@gmail.com>
2026-02-18 16:21:48 +0530

    fix missing import for product controllers in routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
+import { getAllProducts, getProduct } from "../controllers/productControllers.js";
+
 const router = express.Router()
 
 router.get('/', getAllProducts)
 router.get('/:id', getProduct)
-router.get('/featured', getFeaturedProducts)
-router.get('/categories', getAllCategories)
+// router.get('/featured', getFeaturedProducts)
+// router.get('/categories', getAllCategories)
 
 export default router
