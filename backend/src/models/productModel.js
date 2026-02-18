17138346f355e842f0a79e2b2f27a5e0b34361e1
Harshitpant12 <harshpant778@gmail.com>
2026-02-18 14:27:33 +0530

    add product model

---
+++ backend/src/models/productModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            default: ""
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        stock_quantity: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        images: {
+            type: [String], // array of images url
+            default: [],
+        },
+        reviews: [
+            {
+                user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+                rating: { type: Number, min: 1, max: 5 },
+                comment: String,
+                createdAt: { type: Date, default: Date.now() } // if adding update comment option then also add the updatedAt
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model("Product", productSchema)
+
+export default Product
