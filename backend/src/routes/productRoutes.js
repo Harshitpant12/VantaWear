8d8082080ba325a14ee135f7639fa43d37bcc907
Harshitpant12 <harshpant778@gmail.com>
2026-02-19 16:45:52 +0530

    fix missing import and route order

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
 
-import { createProduct, getAllCategories, getAllProducts, getFeaturedProducts, getProduct, updateProduct } from "../controllers/productControllers.js";
+import { createProduct, deleteProduct, getAllCategories, getAllProducts, getFeaturedProducts, getProduct, updateProduct } from "../controllers/productControllers.js";
 import { protectRoute } from "../middlewares/authMiddleware.js";
 import { checkUserRole } from "../middlewares/checkRoleMiddleware.js";
 
 const router = express.Router()
 
 router.get('/', getAllProducts)
-router.get('/:id', getProduct)
 router.get('/featured', getFeaturedProducts)
 router.get('/categories', getAllCategories)
+router.get('/:id', getProduct) // this route should be at the end of all get routes because it can conflict with other get routes like /featured or /categories if placed before them as it will treat "featured" and "categories" as id and will try to find the product with those ids and will return 404 not found error for those routes which is not what we want. So always keep the dynamic routes at the end after all the specific routes to avoid such conflicts.
 
 // admins only routes
 router.post('/', protectRoute, checkUserRole, createProduct)
