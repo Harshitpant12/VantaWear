15b3951aed73d00dec54d778df9cac82207466a4
Harshitpant12 <harshpant778@gmail.com>
2026-02-20 16:40:54 +0530

    implement getMyOrders, getOrderById controller and add route for controllers

--- backend/src/controllers/orderControllers.js
+++ backend/src/controllers/orderControllers.js
@@ -16,11 +16,11 @@ export const createOrder = async (req, res) => {
             } catch {
                 return res.status(400).json({ message: "Invalid order items or shipping address data" })
             }
-            if(!orderItems.length) return res.status(400).json({message: "Order items cannot be empty"})
-            if(!shippingAddress || Object.keys(shippingAddress).length === 0) return res.status(400).json({message: "Shipping address is required"})
+            if (!orderItems.length) return res.status(400).json({ message: "Order items cannot be empty" })
+            if (!shippingAddress || Object.keys(shippingAddress).length === 0) return res.status(400).json({ message: "Shipping address is required" })
 
             const totalPrice = Number(paymentData.metadata.total_price)
-            if(isNaN(totalPrice) || totalPrice < 0) return res.status(400).json({message: "Invalid total price"})
+            if (isNaN(totalPrice) || totalPrice < 0) return res.status(400).json({ message: "Invalid total price" })
             const order = await Order.create({
                 user_id: paymentData.metadata.user_id,
                 order_items: orderItems,
@@ -29,11 +29,41 @@ export const createOrder = async (req, res) => {
                 order_status: "Processing",
                 total_price: Number(paymentData.metadata.total_price)
             })
-            return res.status(200).json({ message: "Order created successfully!", order})
+            return res.status(200).json({ message: "Order created successfully!", order })
         }
         res.status(400).json({ message: "Please make sure your payment is completed" })
     } catch (error) {
         console.log("Error in createOrder controller : ", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const getMyOrders = async (req, res) => {
+    const userId = req.user._id
+    try {
+        const myOrders = await Order.find({ user_id: userId }).sort({ createdAt: -1 }) // present to past (current on top, first one on bottom)
+
+        res.status(200).json({
+            success: true,
+            count: myOrders.length,
+            orders: myOrders
+        })
+    } catch (error) {
+        console.log("Error in getMyOrders controller : ", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const getOrderById = async (req, res) => {
+    const userId = req.user._id
+    const orderId = req.params.id
+    try {
+        const order = await Order.findOne({ user_id: userId, _id: orderId })
+        if (!order) return res.status(404).json({ message: "No order found!" })
+
+        res.status(200).json(order)
+    } catch (error) {
+        console.log("Error in getOrderById controller : ", error.message)
         res.status(500).json({ message: "Internal Server Error" })
     }
 }
--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createOrder } from "../controllers/orderControllers";
+import { createOrder, getMyOrders, getOrderById } from "../controllers/orderControllers.js";
+import { protectRoute } from "../middlewares/authMiddleware.js";
 
 const router = express.Router()
 
-router.post("/", createOrder)
-// router.get('/myorders', getMyOrders)
-// router.get('/:id', getOrderById)
+router.post("/", createOrder) // no need for protect route as that will be triggered by stripe webhook
+router.get('/myorders', protectRoute, getMyOrders)
+router.get('/:id', protectRoute, getOrderById)
 
 export default router
