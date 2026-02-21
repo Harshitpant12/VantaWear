6da29efdc1b26d3f8126c5af6156159401998421
Harshitpant12 <harshpant778@gmail.com>
2026-02-21 19:44:25 +0530

    add hero banner in home page, add favicon in the index.html

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Model from "../../assets/model.png";
+import femaleModel from "../../assets/femaleModel.png";
 
 function Home() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-blue-600 to-purple-700">
-        <div className="text-white text-4xl font-[poppins-light]">
-          Welcome to VantaWear
+      {/* section 1 -> Hero Banner */}
+      <div className="relative flex flex-col lg:flex-row min-h-screen">
+        {/* Left */}
+        <div className="flex-1 flex justify-center items-center bg-amber-300">
+          <img src={Model} alt="" className="w-full h-full object-cover" />
+        </div>
+        {/* Right */}
+        <div className="flex-1 flex justify-center items-center bg-red-300">
+          <img
+            src={femaleModel}
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+        {/* Center */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center pointer-events-none">
+          <div className="pointer-events-auto flex flex-col items-center gap-4">
+            <h1 className="text-white text-5xl font-bold drop-shadow-lg">
+              OWN THE SILENCE
+            </h1>
+            <button className="px-8 py-3 bg-white text-black font-semibold rounded-full hover:bg-gray-200 transition cursor-pointer">
+              Shop Now
+            </button>
+          </div>
         </div>
       </div>
     </>
