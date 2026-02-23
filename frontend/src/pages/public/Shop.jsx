6461c20cb393d7a7db27dd9fe48e52071066bfea
Harshitpant12 <harshpant778@gmail.com>
2026-02-23 12:13:53 +0530

    add header section in shop page

--- frontend/src/pages/public/Shop.jsx
+++ frontend/src/pages/public/Shop.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 function Shop() {
   return (
-    <>
-      <div>Hello from shopping!</div>
-      <p>Hey!!!</p>
-    </>
+    <div className="min-h-screen bg-white">
+      {/* section 1 -> Header */}
+        <div className="w-full bg-white py-16 md:py-24 border-b border-gray-200 flex items-center justify-center">
+          <h1 className="text-black font-black text4xl md:text-6xl uppercase tracking-tighter">ALL WEAR</h1>
+        </div>
+      {/* section 2 -> Layout & Sidebar */}
+    </div>
   );
 }
 
